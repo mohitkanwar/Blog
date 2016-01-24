fcbb264b88d740a2e6633d052e4e06acab972044
Mohit Kanwar <mkanwar@beachbody.com>
2016-01-24 19:29:00 +0530

    removing redundant archive tag

--- archive.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){$.getJSON("/allpages.json",function(B){var A=[];$.each(B.pages,function(C,D){A.push("<li><a href="+D.page.uri+">"+D.page.title+"</a></li>")});$("#list").append(A)})});
