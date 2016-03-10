b972ab07d553e482bb4f689be2c0fa8c703bdeba
Mohit Kanwar <mkanwar@beachbody.com>
2016-03-10 16:25:50 +0530

    Introducing tutorials

---
+++ tutorials/Browser-Game-Tutorial/chapter-1/firstDrawing.js
@@ -0,0 +1,1 @@
+var firstDrawingCanvas=document.getElementById("firstDrawing");var firstDrawingContext=firstDrawingCanvas.getContext("2d");firstDrawingContext.fillStyle="#FFE4C4";firstDrawingContext.fillRect(10,20,150,75);
